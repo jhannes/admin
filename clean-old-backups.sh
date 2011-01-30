abc481f04fb9bee8e1afa8582ae4fb19f96f903b
Johannes Brodwall <johannes@brodwall.com>
2011-01-30 17:48:20 +0100

    Debugging

--- clean-old-backups.sh
+++ clean-old-backups.sh
@@ -7,18 +7,23 @@
 # Requires the following environment variable to be set
 #  BACKUP_DIR
 #  FILE_PATTERN
-#  Optional: BACKUP_ROOT (default dirname BACKUP_DIR
 #  Optional: ARCHIVE_COUNT how many backups to keep (default 7)
 
+[ -z "$BACKUP_DIR" ] && exit 1
+[ -z "$FILE_PATTERN" ] && exit 1
+
 archive_count=${ARCHIVE_COUNT:-7}
-backup_root=${BACKUP_ROOT:-`dirname BACKUP_DIR`}
+backup_root=`dirname BACKUP_DIR`
+
 
 # Delete all but the newest $archive_count backups
+echo "Cleaning $BACKUP_DIR for $FILE_PATTERN"
 (cd $BACKUP_DIR && [ -n "`ls -tr $FILE_PATTERN | head --lines=-$archive_count`" ] && ls -tr $FILE_PATTERN | head --lines=-$archive_count | xargs rm)
 
 # Delete all but the latest file in previous months backup
 for dir in `ls -d $backup_root/* | head --lines=-1`
 do
-  (cd $dir; [ -n "`ls -tr $file_pattern | head --lines=-1`" ] && ls -tr $file_pattern | head --lines=-1 | xargs rm)
+  echo "Cleaning $dir for $FILE_PATTERN"
+  (cd $dir && [ -n "`ls -tr $file_pattern | head --lines=-1`" ] && ls -tr $file_pattern | head --lines=-1 | xargs rm)
 done
 
