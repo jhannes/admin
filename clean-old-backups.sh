43676ef14afab9dccdb9f3fa20f6a53b30903c79
Johannes Brodwall <johannes@brodwall.com>
2011-01-30 17:27:06 +0100

    Typo

--- clean-old-backups.sh
+++ clean-old-backups.sh
@@ -11,10 +11,10 @@
 #  Optional: ARCHIVE_COUNT how many backups to keep (default 7)
 
 archive_count=${ARCHIVE_COUNT:-7}
-backup_root=${BACKUP_ROOT:-`dirname BACKUP_DIR}
+backup_root=${BACKUP_ROOT:-`dirname BACKUP_DIR`}
 
 # Delete all but the newest $archive_count backups
-(cd $BACKUP_DIR; [ -n "`ls -tr $FILE_PATTERN | head --lines=-$archive_count`" ] && ls -tr $FILE_PATTERN | head --lines=-$archive_count | xargs rm)
+(cd $BACKUP_DIR && [ -n "`ls -tr $FILE_PATTERN | head --lines=-$archive_count`" ] && ls -tr $FILE_PATTERN | head --lines=-$archive_count | xargs rm)
 
 # Delete all but the latest file in previous months backup
 for dir in `ls -d $backup_root/* | head --lines=-1`
